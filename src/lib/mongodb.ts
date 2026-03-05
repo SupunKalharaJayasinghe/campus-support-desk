d152f10fda3620959b3ea5bb9dea2e90d45f161e
PabodaWA <wampaboda@gmail.com>
2026-03-05 23:32:00 +0530

    dbcommection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,25 +1,25 @@
-import { MongoClient } from 'mongodb';
+import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI;
-if (!uri) throw new Error('Missing MONGODB_URI in .env.local');
+const MONGODB_URI = process.env.MONGODB_URI as string;
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-declare global {
-  // eslint-disable-next-line no-var
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in .env.local");
 }
 
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
-    global._mongoClientPromise = client.connect();
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
   }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
-}
 
-export default clientPromise;
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
